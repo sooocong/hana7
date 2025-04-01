d1a8021af7c154339acbe364ee005ac711828322
soobin <subin3620@naver.com>
2025-04-01 10:19:01 +0900

    html first commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        mintleaf: "#00b894",
+        carrot: "e67e222",
+        greensea: "#16a085",
+      },
+    },
+  },
+};
